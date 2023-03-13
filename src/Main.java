f5c9edc0dad76f1bdd7990437ece3eb2d3d7456b
nurbek <nurbek07072007@gmail.com>
2023-03-13 19:58:39 +0600

    nurbek commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        nurbek infa = new nurbek();
+        infa.name = "Nurbek";
+        infa.yo = 15;
+        infa.pol = "man";
+        infa.address = "Turgeneva";
+        infa.schoolnumber = 28;
+        infa.clas = 9;
+        infa.maritalStatus = "Very nice";
+
+        System.out.println("=========================\n" + "Name: " + infa.name + "\nYears old: " + infa.yo +
+        "\nSex: " + infa.pol + "\nAddress: " + infa.address + "\nSchool number: " + infa.schoolnumber +
+        "\nClass: " + infa.clas + "\nMarital status: " + infa.maritalStatus + "\n=========================");
     }
 }
