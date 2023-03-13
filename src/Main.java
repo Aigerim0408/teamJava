73e600afd69572cdc8f2b25a51027cb1a20f0228
nurbek <nurbek07072007@gmail.com>
2023-03-13 19:49:22 +0600

    nurbek commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Aigerim");
-        System.out.println("Salam");
     }
 }
---
+++ src/nurbek.java
@@ -0,0 +1,9 @@
+public class nurbek {
+    String name;
+    int yo;
+    String pol;
+    String address;
+    int schoolnumber;
+    int clas;
+    String maritalStatus;
+}
