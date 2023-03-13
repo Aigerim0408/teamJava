75d76a9adb9859a3d1f2e1bac988cb45fb21d087
User <egemberdievasafia@gmail.com>
2023-03-13 19:44:17 +0600

    safia commit

---
+++ src/safia.java
@@ -0,0 +1,12 @@
+public class safia {
+    String name;
+    String address;
+    String currentPlaceOfWork;
+    int age;
+    String school;
+    int height;
+    String Pet;
+    int weight;
+
+
+}
